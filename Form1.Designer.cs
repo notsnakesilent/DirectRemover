9307b41241d8b024faa056554200e8cb599a0a50
Brian Walker <69158149+notsnakesilent@users.noreply.github.com>
2024-01-29 01:36:02 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace DirectRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(85, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 32);
+            this.panel1.TabIndex = 1;
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.ItemHeight = 15;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(85, 103);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.Size = new System.Drawing.Size(262, 21);
+            this.guna2ComboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(137, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selecciona unidad a desinfectar";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 437);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.guna2Button1.BorderRadius = 10;
+            this.guna2Button1.BorderThickness = 1;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(85, 184);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(262, 25);
+            this.guna2Button1.TabIndex = 5;
+            this.guna2Button1.Text = "Comenzar";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // log
+            // 
+            this.log.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.log.ForeColor = System.Drawing.Color.White;
+            this.log.Location = new System.Drawing.Point(20, 233);
+            this.log.Name = "log";
+            this.log.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(391, 152);
+            this.log.TabIndex = 6;
+            this.log.Text = "Processed";
+            this.log.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.panel2.Location = new System.Drawing.Point(20, 232);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(391, 154);
+            this.panel2.TabIndex = 7;
+            this.panel2.Visible = false;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.AnimateWindow = true;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            this.guna2BorderlessForm1.ShadowColor = System.Drawing.Color.Orange;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(405, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(387, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "-";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(432, 433);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "DirectRemover";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.InteropServices;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+namespace DirectRemover
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public DateTime now = DateTime.Now;
+
+        public static List<string> infectados = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.IndianRed;
+            DriveInfo[] unidades = DriveInfo.GetDrives();
+
+            foreach (DriveInfo unidad in unidades)
+            {
+                if (unidad.IsReady)
+                {
+                    string nombreEtiqueta = !string.IsNullOrEmpty(unidad.VolumeLabel)
+                        ? $" ({unidad.VolumeLabel})"
+                        : "";
+
+                    guna2ComboBox1.Items.Add($"{unidad.Name} {nombreEtiqueta}");
+                }
+            }
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            this.WindowState= FormWindowState.Minimized;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            log.Visible = true;
+            panel2.Visible= true;
+            var t = new Thread(Deinfect);
+            t.Name = "Cleaner";
+            t.Priority = ThreadPriority.AboveNormal;
+            t.Start();
+
+        }
+
+        private void Deinfect()
+        {
+            string unidadSeleccionada = guna2ComboBox1.SelectedItem.ToString().Split(' ')[0];
+            log.Text = $"[{now.ToString("HH:mm:ss")}] Obteniendo archivos EXE en la unidad {unidadSeleccionada} , esto puede llevar tiempo";
+            string[] archivosExe = Directory.GetFiles(unidadSeleccionada, "*.exe", SearchOption.AllDirectories);
+            log.Text = "";
+            foreach (string exe in archivosExe)
+            {
+                try
+                {
+                    var fileInfo = new System.IO.FileInfo(exe);
+                    if(fileInfo.Length.ToString().Equals("534016"))
+                    {
+                        using (var fileStream = new FileStream(exe, FileMode.Open, FileAccess.Read))
+                        {
+
+                            var peFile = new PeNet.PeFile(fileStream);
+
+                       if(peFile.ImpHash.Equals("9a06f0024c1694774ae97311608bab5b"))
+                        {
+                            infectados.Add(exe);
+                        }
+                       fileStream.Close();
+                         }
+                    }
+
+
+                    foreach (string inf in infectados)
+                    {
+                        try
+                        {
+                            File.Delete(exe);
+                            log.Text = $"{Environment.NewLine}[{now.ToString("HH:mm:ss")}] Se removio {exe} infectado";
+                            continue;
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Text = $"{Environment.NewLine}[{now.ToString("HH:mm:ss")}] Error {ex.Message} al borrar";
+                            continue;
+                        }
+                    }
+
+
+                    FileAttributes atributos = File.GetAttributes(exe);
+                        bool tieneLosTresAtributos = (atributos & FileAttributes.Hidden) == FileAttributes.Hidden &&
+                        (atributos & FileAttributes.System) == FileAttributes.System;
+                        if(tieneLosTresAtributos)
+                        {
+                            atributos &= ~(FileAttributes.Hidden | FileAttributes.ReadOnly | FileAttributes.System);
+                            File.SetAttributes(exe, atributos);
+                            log.Text = Environment.NewLine+"[" + now.ToString("HH: mm:ss") + "]" + " Se le removieron los artributos a " + exe;
+                        }
+                      
+
+                
+                }
+                catch (Exception ex)
+                {
+                    log.Text = $"{Environment.NewLine}[{now.ToString("HH:mm:ss")}] Error {ex.Message} al parsear";
+                    continue;
+                }
+            }
+
+        }
+    }
+}
